cfb88e2a97c7380cde75c90adcb297e2edb41109
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2021-10-02 14:10:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git extra-feeds https://github.com/powerwheel/extra-feeds' feeds.conf.default
 git clone https://github.com/wzz1190/rt2860v2 package/rt2860v2
-#git clone https://github.com/wuqiong/rt2860v2-for-openwrt-mt7620 package/rt2860v2
-git clone https://github.com/wzz1190/openwrt-luci/tree/master/ralink package/ralink
+#git clone https://github.com/wzz1190/openwrt-luci/tree/master/ralink package/ralink
 #git clone https://github.com/wzz1190/rt2860v2-1 package/rt2860v2
 #src-git extra-feeds https://github.com/powerwheel/extra-feeds.git
 
